9d49167866e2f22f56c0f3c9878753a11ff0371b
SAI KRISHNA KOTHA <kotha.sai.krishna.in@gmail.com>
2025-06-13 11:49:17 +0530

    admin default login page overridden with our site login page

--- easyads/urls.py
+++ easyads/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from app1_users.views import home,testing
+from app1_users.views import home,testing,signin
 urlpatterns = [
+    path('admin/login/', signin,name="signin"),
     path('admin/', admin.site.urls),
     path('', home, name='home'),  # Common home
     path('users/', include('app1_users.urls')),
